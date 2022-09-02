737715d12bced61168c40e5948eb9e222ffca0e3
Juan Antonio <javillar@gmail.com>
2022-09-01 22:45:40 -0500

    cuarta  version entregable incluye reiniciar y continuar despues de salirse y mantiene el score  ya completamente funcional esto ya esta para entregars

--- javascript/indexnstoragenew.js
+++ javascript/indexnstoragenew.js
@@ -33,6 +33,7 @@ window.addEventListener('load', ()=> {
 }
 
 )
+let reinicio = confirm("Iniciar un nuevo Draw (OK)  continuo Draw Actual (Cancel)");
 
 // a partir de la posicion 1 el contenido es el proximo juego para los games de la seguientes ronda 
 const reglas =['',9,9,10,10,11,11,12,12,13,13,14,14,15,15,"campeon"]
@@ -51,7 +52,7 @@ const reglas =['',9,9,10,10,11,11,12,12,13,13,14,14,15,15,"campeon"]
     console.log(res);
     return res.json();
   }).then(function(data){
-    console.log(data)
+    //console.log(data)
   }).catch(function(error){
     console.log(error);
   })
@@ -69,78 +70,196 @@ function getGamesFromStoorage() {
         id: '1',
         nameP1: 'Raul Ramirez',
         nameP2: 'Vitas Gerulaitis',
-        winner: ''
+        winner: '',
+        score1: '0',
+        score2: '0',
+        score3: '0',
+        score4: '0',
+        score5: '0',
+        score6: '0',
+        desabilitado : false,
     },
     {
         id: '2',
         nameP1: 'Jimmy Connors',
         nameP2: 'Adriano Panatta',
-        winner: ''
+        winner: '',
+        score1: '0',
+        score2: '0',
+        score3: '0',
+        score4: '0',
+        score5: '0',
+        score6: '0',
+        desabilitado :false,
     },
     {
         id: '3',
         nameP1: 'Gillermo Vilas',
         nameP2: 'Ivan Lendl',
+        winner: '',
+        score1: '0',
+        score2: '0',
+        score3: '0',
+        score4: '0',
+        score5: '0',
+        score6: '0',
+        desabilitado : false,
     },
     {
         id: '4',
         nameP1: 'Rosco Tanner',
         nameP2: 'Ile Nastase',
+        winner: '',
+        score1: '0',
+        score2: '0',
+        score3: '0',
+        score4: '0',
+        score5: '0',
+        score6: '0',
+        desabilitado : false,
     },
     {
         id: '5',
         nameP1: 'Manuel Orantes',
         nameP2: 'Brian Gottfried',
+        winner: '',
+        score1: '0',
+        score2: '0',
+        score3: '0',
+        score4: '0',
+        score5: '0',
+        score6: '0',
+        desabilitado : false,
     },
     {
         id: '6',
         nameP1: 'Harold Solomon',
         nameP2: 'Jhon Mcnroee',
+        winner: '',
+        score1: '0',
+        score2: '0',
+        score3: '0',
+        score4: '0',
+        score5: '0',
+        score6: '0',
+        desabilitado : false,
     },
     {
         id: '7',
         nameP1: 'Stefan Edberg',
         nameP2: 'Mats Wilander',
+        winner: '',
+        score1: '0',
+        score2: '0',
+        score3: '0',
+        score4: '0',
+        score5: '0',
+        score6: '0',
+        desabilitado : false,
     },
     {
         id: '8',
         nameP1: 'Eduardo Velez',
         nameP2: 'Andre Agassi',
+        winner: '',
+        score1: '0',
+        score2: '0',
+        score3: '0',
+        score4: '0',
+        score5: '0',
+        score6: '0',
+        desabilitado : false,
     },
     {
         id: '9',
         nameP1: 'Raul Ramirez',
         nameP2: 'Jimmy Connors',
+        winner: '',
+        score1: '0',
+        score2: '0',
+        score3: '0',
+        score4: '0',
+        score5: '0',
+        score6: '0',
+        desabilitado : true,
     },
     {
         id: '10',
         nameP1: 'Ivan Lendl',
         nameP2: 'Ile Nastase',
+        winner: '',
+        score1: '0',
+        score2: '0',
+        score3: '0',
+        score4: '0',
+        score5: '0',
+        score6: '0',
+        desabilitado : true,
     },
     {
         id: '11',
         nameP1: 'Manuel Orantes',
         nameP2: 'John Mcnroee',
+        winner: '',
+        score1: '0',
+        score2: '0',
+        score3: '0',
+        score4: '0',
+        score5: '0',
+        score6: '0',
+        desabilitado : true,
     },
     {
         id: '12',
         nameP1: 'Mats Wilander',
         nameP2: 'Eduardo Velez',
+        winner: '',
+        score1: '0',
+        score2: '0',
+        score3: '0',
+        score4: '0',
+        score5: '0',
+        score6: '0',
+        desabilitado : true,
     },
     {
         id: '13',
         nameP1: 'Raul Ramirez',
         nameP2: 'Ile Nastase',
+        winner: '',
+        score1: '0',
+        score2: '0',
+        score3: '0',
+        score4: '0',
+        score5: '0',
+        score6: '0',
+        desabilitado : true,
     },
     {
         id: '14',
         nameP1: 'Manuel Orantes',
         nameP2: 'Eduardo Velez',
+        winner: '',
+        score1: '0',
+        score2: '0',
+        score3: '0',
+        score4: '0',
+        score5: '0',
+        score6: '0',
+        desabilitado : true,
     },
     {
         id: '15',
         nameP1: 'Pending',
         nameP2: 'Pending',
+        winner: '',
+        score1: '0',
+        score2: '0',
+        score3: '0',
+        score4: '0',
+        score5: '0',
+        score6: '0',
+        desabilitado : true,
     },
     {
       id: '16',
@@ -150,12 +269,17 @@ function getGamesFromStoorage() {
 ]
 
 // generar firstRound en localStorage
-localStorage.setItem('gamesFirstRound',JSON.stringify(games))
-const items = JSON.parse(localStorage.getItem('gamesFirstRound'))
+localStorage.setItem('gamesFirstRound',JSON.stringify(games)) // de aqui se carga todo la primera vez
+let items = JSON.parse(localStorage.getItem('gamesFirstRound'))
 return items
 }
-// ir a storage por set up inicial
-items =getGamesFromStoorage()
+// ir a storage por set up inicial una flag si ya tiene info y no ir hacer el setItems
+if (reinicio){
+  items =getGamesFromStoorage() // esto pone en ceros los scores
+}
+else{
+  items = JSON.parse(localStorage.getItem('gamesFirstRound')) //ojo sirve?
+}
 // se obtiene el proximo match del ganador del arreglo de reglas cada posicion indica cual es el proximo juego del winner
 function buscarNextRound(g,w) {
   return reglas[g];
@@ -165,6 +289,8 @@ let player2 = '';
 idGame=0;
 const appDiv = document.getElementById('app');
 // generar first rorund
+// ojo agregue
+games= items
 let i =1
 generaBotones()
 // fin generar first rorund
@@ -235,7 +361,7 @@ actualGame['score2'] = score2
 actualGame['score4'] = score4
 actualGame['score6'] = score6
 // aqui puedo actualizar score tendre el score del ganador o perdedor con el del perdedor podre deducuir el del ganador (update pendiente funciono en otra parte)
-updateStorageTemporal(actualGame,id-1,x) 
+updateStorageTemporal(actualGame,id-1)
   // -----------(se determina la posicion del proximo juego en base a si fue par o non)
  pos1 = (id-2) <= 0 ? 0 :id-2
  pos2 = (id-1) ==0 ? 1 :id-1
@@ -279,10 +405,8 @@ switch (id) {
   default:
     break;
 }
-
-p1= games[pos1].winner
-p2= games[pos2].winner
-nextGame[nextRound]={"id":nextRound+'',"nameP1":games[pos1].winner,"nameP2":games[pos2].winner}
+let desabilitar = false //ojo
+nextGame[nextRound]={"id":nextRound+'',"nameP1":games[pos1].winner,"nameP2":games[pos2].winner,"winner":'',"score1":games[pos1],"score2":games[pos2],"score3":games[pos1],"score4":games[pos2],"score5":games[pos1],"score6":games[pos2]} // si agarro el 1 el 2 es pending
 if(id+''=='15'){
   //campeon = document.getElementsByClassName('item16'); //no quiso 
   campeon=document.querySelector(".item"+(16+''));     // si quiso
@@ -294,20 +418,36 @@ if(id+''=='15'){
         <p id=P${((id*2)-1+'')}> ${games[id-1].nameP1} </p>
         <p>vs</p>
         <p id=P${(id*2+'')}> ${games[id-1].nameP2} </p>
+        <p>score</p>
         <p> ${games[id-1].score1+ '-'+games[id-1].score2+'  '+games[id-1].score3+ '-'+games[id-1].score4+'  '+games[id-1].score5+ '-'+games[id-1].score6} </p>
         </button>
         `
 }
 else {
-  // validar no haya algun player pending poner alerta con Toastify si uno de los 2 es undefined or pending
   // prender una flag y con eso saber si es disabled or enabled
   let desabilitar = false
-  if(games[pos1].winner ===undefined || games[pos2].winner ===undefined || games[pos2].winner ==='' || games[pos1].winner ===''){
+  if(games[pos1].winner ===undefined  || games[pos1].winner ===''){
     desabilitar = true
   }
+  if(games[pos2].winner ===undefined || games[pos2].winner ===''){
+    desabilitar = true
+  }
+  
+  if(games[pos1].winner.length > 1){
+    games[pos1].desabilitado = true
+    desabilitar = true
+  }
+  if(games[pos2].winner.length > 1){
+    games[pos2].desabilitado = true
+    desabilitar = true
+  }
+  if(games[id-1].nameP1=='' || games[id-1].nameP2==''){
+    
+  }
+  
   // no iba a terminar sin usar esto  ${desabilitar && "disabled"}
   x.innerHTML = `
-        <button type="button" class="btn btn-primary showBtn" ${desabilitar && "disabled"} id="${nextRound+''}" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        <button type="button" class="btn btn-primary showBtn" "disabled" id="${nextRound+''}" data-bs-toggle="modal" data-bs-target="#exampleModal">
         <p id=P${(((nextRound*2)-1)+'')}> ${games[pos1].winner ===undefined ? "Pending":games[pos1].winner} </p>
         <p>vs</p>
         <p id=P${((nextRound*2)+'')}> ${games[pos2].winner===undefined ? "Pending":games[pos2].winner}  </p>
@@ -323,36 +463,39 @@ else {
         <p id=P${((id*2)-1+'')}> ${games[id-1].nameP1} </p>
         <p>vs</p>
         <p id=P${(id*2+'')}> ${games[id-1].nameP2} </p>
+        <p>score</p>
         <p> ${games[id-1].score1+ '-'+games[id-1].score2+'  '+games[id-1].score3+ '-'+games[id-1].score4+'  '+games[id-1].score5+ '-'+games[id-1].score6} </p>
         </button>
         `
-        
-        
 }
   
  updateStorage(nextGame,nextRound,id-1)
 })
 
 function generaBotones(){
-    items.forEach((item) => {
+  games.forEach((item) => {
+   // items.forEach((item) => {
       if((i+'')=='31' || (i+'') =='32' ){
-        appDiv.innerHTML += `<div class="item${item.id}"> <p id=P${(i+'')}>  ${""}</p> </div>` // tenia pending
+        appDiv.innerHTML += `<div class="item${item.id}"> <p id=P${(i+'')}>  ${""}</p> </div>` 
       }
       else {
         // se incluyo ternario para poner disabled los matches de las siguientes rondas pudo usar operadores AND y solo cuando sea verdaddera la condicion poner el disabled
+        // <button type="button" class="btn btn-primary showBtn" id="${item.id}" ${parseInt(item.id) > 8 ? "disabled":"enabled"} data-bs-toggle="modal" data-bs-target="#exampleModal">
         appDiv.innerHTML += `
             <div class="item${item.id}">
-              <button type="button" class="btn btn-primary showBtn" id="${item.id}" ${parseInt(item.id) > 8 ? "disabled":"enabled"} data-bs-toggle="modal" data-bs-target="#exampleModal">
+              <button type="button" class="btn btn-primary showBtn" id="${item.id}" ${(item.desabilitado === true ? "disabled":"enabled")} data-bs-toggle="modal" data-bs-target="#exampleModal">
               <p id=P${(i+'')}> ${item.nameP1} </p>
               <p>vs</p>
               <p id=P${((i+1)+'')}> ${item.nameP2} </p>
-              <p> score</p>
+             <p> score </p>
+             <p> ${item.score1+ ' '+item.score2+' '+item.score3+' '+item.score4+' '+item.score5+' '+item.score6} </p> 
               </button>
             </div>`;
         i=i+2;
       }});
     
 }
+//<p> ${(item.score1 ='undefined') ? ' ' :item.score1 + '-'+ (item.score2='undefined') && ' ' +'  '+(item.score3='undefined') && ' '+ '-'+(item.score4='undefined') && ' '+'  '+(item.score5='undefined') && ' '+ '-'+(item.score6='undefined') && ' '} </p>
 function generaListeners(){
     for (const btn of showButtons) {
         btn.addEventListener('click', (e) => {
@@ -454,13 +597,24 @@ function generaListeners(){
   }
 }
 // args son arreglo donde di click y el id con este puedo ir a reglas a traer el nextMatch
-function updateStorageTemporal(a,b,x){
+function updateStorageTemporal(a,b){
   games[b] = a // actualizar player en siguientematch 
 }
 function updateStorage(a,b){
     games[b-1] = a[b]
+    games[b-1].desabilitado =false
+    games[b-1].winner = a[b].winner
+    games[b-1].score1 = a[b].score1='0'
+    games[b-1].score2 = a[b].score2='0'
+    games[b-1].score3 = a[b].score3='0'
+    games[b-1].score4 = a[b].score4='0'
+    games[b-1].score5 = a[b].score5='0'
+    games[b-1].score6 = a[b].score6='0'
+    console.log("---- en storage ---------")
     localStorage.setItem('gamesFirstRound',JSON.stringify(games)) 
     items = JSON.parse(localStorage.getItem('gamesFirstRound')) //solo quite const y jalo sera la inmutabilidad ?
+    // games = items //ojo
+    console.log("---- en storage ---------")
    generaListeners()
   
   }
